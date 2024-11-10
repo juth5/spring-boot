91905a7b6b1fd8d3a6f4dbd6edb72b1308592f0d
深川　貴司 <fukagawatakashiee893@gmail.com>
2024-11-10 11:30:27 +0900

    db

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,12 +2,13 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
+
     @GetMapping("/hello")
     public String sayHello() {
-        return "hello";
+        return "Hello, World!";
     }
-
 }
