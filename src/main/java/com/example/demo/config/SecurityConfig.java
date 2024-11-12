8043a03957f69efc5bf70df5c3f553560ef473c8
深川　貴司 <fukagawatakashiee893@gmail.com>
2024-11-13 00:18:43 +0900

    auth

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -6,7 +6,11 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 public class SecurityConfig {
-
+    //こちらでsecurityFilterChainメソッドをカスタマイズしている
+    //Springコンテナが読んでいる
+    //つまり、このメソッドは内部で呼ばれるメソッドを定義している
+    //このメソッドの主な目的は、アプリケーションのリクエストに対する認証・認可ポリシーを設定することです。
+    //Spring Bootでセキュリティ設定をさらにカスタマイズしたい場合は、SecurityFilterChain を定義して、Spring Securityの設定を追加・変更できます。
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
@@ -24,3 +28,4 @@ public class SecurityConfig {
         return http.build();
     }
 }
+
