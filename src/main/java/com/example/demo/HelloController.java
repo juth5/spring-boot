96edc8dc990f1adf2fe8fc27df36fd550d87041b
深川　貴司 <fukagawatakashiee893@gmail.com>
2024-11-14 23:49:01 +0900

    auth

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -7,18 +7,24 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.demo.entity.Memo;
+import com.example.demo.entity.Users;
 import com.example.demo.mapper.MemoMapper;
+import com.example.demo.mapper.UsersMapper;
 
 @Controller
 public class HelloController {
 
     @Autowired
     MemoMapper memoMapper;
+    @Autowired
+    UsersMapper usersMapper;
 
     @GetMapping("/hello")
     public String sayHello() {
 
         List<Memo> memos = memoMapper.findAll();
+        List<Users> users = usersMapper.findAll();
+        System.out.println(users);
 
         return "hello";
     }
--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.example.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -15,17 +17,22 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/login", "/hello").permitAll()  // 認証不要のパス
+                .antMatchers("/login").permitAll()  // 認証不要のパス
                 .anyRequest().authenticated()           // その他のリクエストは認証が必要
             .and()
             .formLogin()
                 .loginPage("/login")                    // カスタムログインページの指定
+                .defaultSuccessUrl("/hello", true)   // 認証後のリダイレクト先を指定
                 .permitAll()
             .and()
             .logout()
                 .permitAll();
 
         return http.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // BCryptを使用してパスワードをエンコード
     }
 }
 
