0f0250ed57970f9fe2bf4aaf377bfc03cb4f54d0
深川　貴司 <fukagawatakashiee893@gmail.com>
2024-11-17 15:59:06 +0900

    auth

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,23 +13,29 @@ import com.example.demo.entity.Memo;
 import com.example.demo.entity.Users;
 import com.example.demo.mapper.MemoMapper;
 import com.example.demo.mapper.UsersMapper;
+import com.example.demo.service.UserInfoService;
 
 @Controller
 public class HelloController {
 
     @Autowired
     MemoMapper memoMapper;
+
     @Autowired
     UsersMapper usersMapper;
 
-    @GetMapping("/hello")
-    public String sayHello() {
+    @Autowired
+    UserInfoService userInfoService;
 
+    @GetMapping("/hello")
+    public String sayHello(HttpServletRequest request) {
+
+        HttpSession session = request.getSession(false);
         List<Memo> memos = memoMapper.findAll();
         List<Users> users = usersMapper.findAll();
         System.out.println(users);
+        userInfoService.getAuthenticatedUserDetails();
 
         return "hello";
     }
-
 }
