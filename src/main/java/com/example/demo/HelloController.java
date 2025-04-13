4c33891bd3d5e6802153c6143443f7528fff05fe
juth <63380489+juth5@users.noreply.github.com>
2025-04-13 12:02:35 +0900

    Revert "j"

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -6,11 +6,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.example.demo.entity.CustomUserDetails;
 import com.example.demo.entity.Memo;
 import com.example.demo.entity.Users;
 import com.example.demo.mapper.MemoMapper;
@@ -37,11 +35,7 @@ public class HelloController {
         List<Memo> memos = memoMapper.findAll();
         List<Users> users = usersMapper.findAll();
 
-        CustomUserDetails user = userInfoService.getAuthenticatedUserDetails();
-        System.out.println("aaaaaaaaaaa");
-
-        System.out.println(user.getEmail());
-        System.out.println("aaaaaaaaaaa");
+        userInfoService.getAuthenticatedUserDetails();
 
         return "hello";
     }
