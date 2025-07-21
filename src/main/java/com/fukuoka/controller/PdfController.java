77d879c528d84cec79b251c52f3d1945a48303a0
深川　貴司 <fukagawatakashiee893@gmail.com>
2025-07-22 00:16:51 +0900

    レイアウト

--- src/main/java/com/fukuoka/controller/PdfController.java
+++ src/main/java/com/fukuoka/controller/PdfController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
@@ -22,18 +23,62 @@ public class PdfController {
     @Autowired
     private TemplateEngine templateEngine;
 
-    @GetMapping("/pdf")
-    public String pdf() {
-        return "pdf"; // hello.html を返す
+    @GetMapping("/report")
+    public String pdf(Model model) {
+        List<Map<String, String>> rows = List.of(
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+
+            Map.of("name", "aaaaaaaaaaaaaa", "value", "bbbbbbb")
+
+            
+        );
+        model.addAttribute("rows", rows);
+        return "report";
     }
 
 
-    @GetMapping("/report/pdf")
-    public ResponseEntity<byte[]> generatePdf() throws Exception {
-        List<Map<String, String>> rows = List.of(
-            Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
-            Map.of("name", "aaaaaaaaaaaaaa", "value", "bbbbbbb")
-        );
+@GetMapping("/report/pdf")
+public ResponseEntity<byte[]> generatePdf() throws Exception {
+    List<Map<String, String>> rows = List.of(
+        Map.of("name", "aaaaaaaaaa", "value", "bbbbbbbb"),
+        Map.of("name", "aaaaaaaaaaaaaa", "value", "bbbbbbb")
+    );
 
     Context context = new Context();
     context.setVariable("rows", rows);
@@ -42,10 +87,10 @@ public class PdfController {
     ByteArrayOutputStream out = new ByteArrayOutputStream();
     ITextRenderer renderer = new ITextRenderer();
 
-String fontPath = getClass().getClassLoader().getResource("fonts/NotoSansJP-Regular.ttf").getPath();
-System.out.println("FONT PATH = " + fontPath);
+    String fontPath = getClass().getClassLoader().getResource("fonts/NotoSansJP-Regular.ttf").getPath();
+    System.out.println("FONT PATH = " + fontPath);
 
-renderer.getFontResolver().addFont(fontPath, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+    renderer.getFontResolver().addFont(fontPath, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
 
 
     renderer.setDocumentFromString(html);
